fe270ff3892fed9a70fc0025c874e392a16c5b87
houfengjiao <976168182@qq.com>
2021-10-26 15:20:08 +0800

    发布v1.0.0版本

--- example/src/main.js
+++ example/src/main.js
@@ -4,7 +4,8 @@ import router from './router'
 import store from './store'
 import ViewUI from 'view-design'
 import 'view-design/dist/styles/iview.css'
-import vui from '../../src/index'
+import vui from '../../dist/iview-page.umd.js'
+import '../../dist/iview-page.css'
 
 Vue.use(ViewUI)
 Vue.use(vui)
